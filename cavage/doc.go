9aa0c93689c45754a6d9680b45bc86dad10b16c4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-09 09:51:49 -0700

    doc

--- cavage/doc.go
+++ cavage/doc.go
@@ -1,5 +1,7 @@
 /*
-Package cavage implements HTTP Signature signing and verification per draft-cavage-http-signatures-12.
+Package cavage implements HTTP Signature signing and verification per draft-cavage-http-signatures-12:
+
+https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12
 
 This is the version of HTTP signatures used by the Fediverse (Mastodon, GoToSocial, Pleroma/Akkoma, Misskey, Lemmy, etc.) for ActivityPub server-to-server federation.
 
