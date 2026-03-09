0a6f6a0414fe84adb9cdb99147b0dd7b8b2645a8
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-09 09:32:04 -0700

    doc

---
+++ cavage/doc.go
@@ -0,0 +1,6 @@
+/*
+Package cavage implements HTTP Signature signing and verification per draft-cavage-http-signatures-12.
+
+This is the version of HTTP signatures used by the Fediverse (Mastodon, GoToSocial, Pleroma/Akkoma, Misskey, Lemmy, etc.) for ActivityPub server-to-server federation.
+*/
+package cavage
